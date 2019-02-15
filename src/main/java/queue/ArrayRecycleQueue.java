b42d95b6a107b63997e822735035d15bb6e708ee
yuhao <yuhao@daojia-inc.com>
2019-02-15 10:07:39 +0800

    ArrayRecycleQueue

--- src/main/java/queue/ArrayRecycleQueue.java
+++ src/main/java/queue/ArrayRecycleQueue.java
@@ -1,15 +1,50 @@
 package queue;
 
 public class ArrayRecycleQueue<T> implements Queue<T> {
+
+    private int head;
+    private int tail;
+    private T[] data;
+    private int size;
+    private int capacity;
+
+    public ArrayRecycleQueue(int capacity) {
+        this.capacity = capacity+1;
+        data = (T[]) new Object[capacity + 1];
+    }
+
     public int size() {
-        return 0;
+        return size;
     }
 
     public void enqueue(T t) {
-
+        int nextIndex = (tail + 1) % capacity;
+        if (nextIndex == head) {
+            return;
+        }
+        data[tail] = t;
+        tail = nextIndex;
+        size++;
     }
 
     public T dequeue() {
-        return null;
+        if (head == tail) {
+            return null;
+        }
+        T rt = data[head];
+        head = (head + 1) % capacity;
+        return rt;
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new ArrayRecycleQueue<Integer>(3);
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        System.out.println(queue.dequeue());
+        queue.enqueue(4);
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
     }
 }
