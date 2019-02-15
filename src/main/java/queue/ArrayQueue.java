e052bbfb7c4e0134841be3868c98fa9d52c45800
yuh <yuhao941024>
2019-02-15 08:15:08 +0800

    链表实现的无限制队列

---
+++ src/main/java/queue/ArrayQueue.java
@@ -0,0 +1,15 @@
+package queue;
+
+public class ArrayQueue<T> implements Queue<T> {
+    public int size() {
+        return 0;
+    }
+
+    public void enqueue(T t) {
+
+    }
+
+    public T dequeue() {
+        return null;
+    }
+}
---
+++ src/main/java/queue/ArrayRecycleQueue.java
@@ -0,0 +1,15 @@
+package queue;
+
+public class ArrayRecycleQueue<T> implements Queue<T> {
+    public int size() {
+        return 0;
+    }
+
+    public void enqueue(T t) {
+
+    }
+
+    public T dequeue() {
+        return null;
+    }
+}
