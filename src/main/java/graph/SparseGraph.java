aad3431767c6ea1543b057ef12d80f513ba43fc4
yuhao <yuhao@daojia-inc.com>
2019-03-13 10:18:40 +0800

    堆排序

--- src/main/java/graph/SparseGraph.java
+++ src/main/java/graph/SparseGraph.java
@@ -1,7 +1,9 @@
 package graph;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 public class SparseGraph {
 
@@ -46,10 +48,39 @@ public class SparseGraph {
     }
 
 
+    public List<Integer> nDegree(int s, int degree) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(s);
+        boolean[] visited = new boolean[matrix.length];
+        ArrayList<Integer> list2 = new ArrayList<>();
+        int[] distance = new int[matrix.length];
+        distance[s] = 0;
+
+        while (!list.isEmpty()) {
+            Integer remove = list.remove();
+            if (distance[remove] > degree) {
+                break;
+            }
+            if (remove != s) {
+                list2.add(remove);
+            }
+            visited[remove] = true;
+            LinkedList<Integer> list1 = this.matrix[remove];
+            for (Integer integer : list1) {
+                if (!visited[integer]) {
+                    distance[integer] = distance[remove] + 1;
+                    list.add(integer);
+                }
+            }
+        }
+        return list2;
+    }
+
+
     public void dfs(int s, int t) {
         boolean[] visited = new boolean[matrix.length];
         int[] prev = new int[matrix.length];
-        Arrays.fill(prev,-1);
+        Arrays.fill(prev, -1);
         boolean found = _dfs(visited, prev, s, t);
         if (found) {
             print(prev, t);
@@ -73,6 +104,33 @@ public class SparseGraph {
         return false;
     }
 
+    public List<Integer> dfsDegree(int s, int degree) {
+        boolean[] visited = new boolean[matrix.length];
+        ArrayList<Integer> list = new ArrayList<>();
+        _dfsDegree(visited, s, 0, list, degree, s);
+        return list;
+    }
+
+    private void _dfsDegree(boolean[] visited, int s, int curr, List<Integer> list2, int max, int ori) {
+        if (curr > max) {
+            return;
+        }
+        if (s != ori) {
+            list2.add(s);
+        }
+        visited[s] = true;
+        LinkedList<Integer> list = this.matrix[s];
+        //这是同级别的 第一个不行则不行
+        for (Integer integer : list) {
+            if (!visited[integer]) {
+                if (curr >= max) {
+                    break;
+                }
+                _dfsDegree(visited, integer, curr + 1, list2, max, ori);
+            }
+        }
+    }
+
 
     public void print(int[] prev, int t) {
         if (prev[t] != -1) {
@@ -92,9 +150,10 @@ public class SparseGraph {
         sparseGraph.add(0, 1);
         sparseGraph.add(1, 2);
         sparseGraph.add(2, 3);
-        sparseGraph.add(2, 4);
+        sparseGraph.add(3, 4);
 
-        sparseGraph.dfs(0, 4);
+        List<Integer> degree = sparseGraph.dfsDegree(0, 3);
+        System.out.println(degree);
 
 
     }
